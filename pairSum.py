b333f30be7d7b549c0939cf25c9745f9656b6c46
Kam ZC <theblizzzzard@gmail.com>
2020-05-23 01:26:51 -0400

    Update pairSum.py

--- pairSum.py
+++ pairSum.py
@@ -1,7 +1,7 @@
 class PairSum:
     '''
     Given an array of integers, return indices of the two numbers such that they add up to a specific target.
-    You may assume that each input would have exactly one solution, and you may not use the same element twice
+    You may assume that each input would have exactly one solution, and you may not use the same element twice.
     '''
     def __init__(self, arr, k):
         self.arr = arr
