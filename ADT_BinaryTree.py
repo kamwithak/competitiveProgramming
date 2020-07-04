cd0144372168e6803b4ab2e265503ec1eef9e5c2
kamwithak <kamran.choudhry@mail.mcgill.ca>
2020-07-04 19:10:01 -0400

    edited

--- ADT_BinaryTree.py
+++ ADT_BinaryTree.py
@@ -1,35 +1,45 @@
 class Node:
-    def __init__(self, root, left=None, right=None):
-        self.root = root
+    def __init__(self, key, left=None, right=None):
+        self.key = key
         self.left = left
         self.right = right
 
     def insert(self, data):
-        if self.root:
-            if data < self.root:
+        if self.key:
+            if data < self.key:
                 if self.left is None:
                     self.left = Node(data)
                 else:
                     self.left.insert(data)
-            elif data > self.root:
+            elif data > self.key:
                 if self.right is None:
                     self.right = Node(data)
                 else:
                     self.right.insert(data)
         else:
-            self.root = data
+            self.key = data
+
+    def search(self, data):
+        if self.key is None or self.key == data:
+            return self.key
+        if data < self.key: return self.left.search(data)
+        else: return self.right.search(data)
 
     def PrintTree(self):
         if self.left:
             self.left.PrintTree()
-        print(self.root)
+        print(self.key)
         if self.right:
             self.right.PrintTree()
 
 if __name__ == "__main__":
-    root = Node(5)
-    root.insert(1)
-    root.insert(2)
-    root.insert(3)
-    root.insert(4)
-    root.PrintTree()
+    root = Node(50)
+    root.insert(30)
+    root.insert(20)
+    root.insert(40)
+    root.insert(70)
+    root.insert(60)
+    root.insert(80)
+    
+    print(root.search(50))
+    #root.PrintTree()
