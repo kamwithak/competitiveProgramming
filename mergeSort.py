7c8d4c8ba31fb8fc3b40b3c65efd74f0ad87a856
kamwithak <kamran.choudhry@mail.mcgill.ca>
2020-07-12 16:14:10 -0400

    NEW: merge two arrays question added in same file

--- mergeSort.py
+++ mergeSort.py
@@ -56,12 +56,46 @@ class MergeTwoLists():
             self.ans.addLast(self.remove(self.getFirstNode(l1)))
         while (self.getSize(l2) > 0):
             self.ans.addLast(self.remove(self.getFirstNode(l2)))
-        
         return self.ans
 
-if __name__ == "__main__":
-    l1 = ListNode(1, ListNode(2, ListNode(4)))
-    l2 = ListNode(1, ListNode(3, ListNode(4)))
+class MergeTwoArrays():
+    def __init__(self, nums1, nums2, m, n):
+        self.nums1 = nums1
+        self.nums2 = nums2
+        self.m = m
+        self.n = n
+    
+    def merge(self):
+        ans = []
+        while (self.m > 0 and self.n > 0):
+            if (self.nums1[0] < self.nums2[0]):
+                ans.append(self.nums1[0])
+                del self.nums1[0]
+                self.m -= 1
+            else:
+                ans.append(self.nums2[0])
+                del self.nums2[0]
+                self.n -= 1
+        while (self.m > 0):
+            ans.append(self.nums1[0])
+            del self.nums1[0]
+            self.m -= 1
+        while (self.n > 0):
+            ans.append(self.nums2[0])
+            del self.nums2[0]
+            self.n -= 1
+        return ans
 
-    obj = MergeTwoLists(l1, l2)
-    print(obj.getSize(l1))
+    def mergeInPlace(self):
+        pass
+
+if __name__ == "__main__":
+    #l1 = ListNode(1, ListNode(2, ListNode(4)))
+    #l2 = ListNode(1, ListNode(3, ListNode(4)))
+
+    #obj = MergeTwoLists(l1, l2)
+    #print(obj.getSize(l1))
+
+    obj = MergeTwoArrays([1,3,5,7],[0,2,4,6],4,4)
+    print(obj.merge())
+    print(obj.mergeInPlace())
