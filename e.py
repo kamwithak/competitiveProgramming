1e8d92f751ba5135b5dc5305fa1b57da4270608b
kamwithak <kamran.choudhry@mail.mcgill.ca>
2020-07-23 23:44:39 -0400

    NEW: calcualte e approx.

---
+++ e.py
@@ -0,0 +1,19 @@
+'''
+ This is a toy program to print the approximated value of e ~ 2.17828
+=>    1/(n!) + 1/((n-1)!) + 1/((n-2)!) + ... + 1/(3!) + 1/(2!) + 1/(1!) + 1/(0!) = e, as n -> inf 
+# Date: 07/23/2020
+'''
+
+def e(n):
+	def f(n):
+		if n == 1 or n == 0:
+			return 1
+		else:
+			return (n * f(n-1))
+			
+	if n == 0:
+		return 1
+	else:
+		return 1/f(n) + e(n - 1)
+
+print(f"e is approximately {e(99)}")
