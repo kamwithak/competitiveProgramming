5169462aef938fa3bad0773412b0a767851e95ce
kamwithak <kamran.choudhry@mail.mcgill.ca>
2020-07-29 00:09:41 -0400

    EDIT: unit tests added

--- CTCI Solutions/oneAway.py
+++ CTCI Solutions/oneAway.py
@@ -26,11 +26,63 @@ def oneAway(str1: str, str2: str) -> bool:
     return False
 
 def oneEditReplace(str1: str, str2: str) -> bool:
-    ctr = 0
+    found = False
     for i in range(len(str1)):
         if (str1[i] != str2[i]):
-            ctr += 1
-    return ctr <= 1
+            if (found):
+                return False
+            found = not found
+    return True
 
 def oneEditInsert(str1: str, str2: str) -> bool:
-    pass
+    edited = False
+    i = 0
+    j = 0
+    while (i < len(str1) and j < len(str2)):
+        if (str1[i] != str2[j]):
+            if (edited):
+                return False
+            edited = True
+            j += 1
+        else:
+            i += 1
+            j += 1
+    return True
+
+#print(oneEditReplace("abc", "ade"))
+#print(oneEditInsert("a", "abc"))
+
+import unittest
+class Test(unittest.TestCase):
+    '''Test Cases'''
+    data = [
+        ('pale', 'ple', True),
+        ('pales', 'pale', True),
+        ('pale', 'bale', True),
+        ('paleabc', 'pleabc', True),
+        ('pale', 'ble', False),
+        ('a', 'b', True),
+        ('', 'd', True),
+        ('d', 'de', True),
+        ('pale', 'pale', True),
+        ('pale', 'ple', True),
+        ('ple', 'pale', True),
+        ('pale', 'bale', True),
+        ('pale', 'bake', False),
+        ('pale', 'pse', False),
+        ('ples', 'pales', True),
+        ('pale', 'pas', False),
+        ('pas', 'pale', False),
+        ('pale', 'pkle', True),
+        ('pkle', 'pable', False),
+        ('pal', 'palks', False),
+        ('palks', 'pal', False)
+    ]
+
+    def test_oneAway(self):
+        for [test_s1, test_s2, expected] in self.data:
+            actual = oneAway(test_s1, test_s2)
+            self.assertEqual(actual, expected)
+
+if __name__ == "__main__":
+    unittest.main()
