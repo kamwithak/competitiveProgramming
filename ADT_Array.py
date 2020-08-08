8613e0aed409f65228fc60f8eea36e49a689f259
Kam ~ <theblizzzzard@gmail.com>
2020-08-07 22:27:26 -0400

    Update ADT_Array.py

--- ADT_Array.py
+++ ADT_Array.py
@@ -5,7 +5,7 @@ class Array:								# ADT ~ Array of {key, value} arrays
 	# O(n) ~ linear time, inefficient, we can do better ;)
 
 	def printRelation(self):
-		for pair in self.array:						# print each (key, value) pair
+		for pair in self.array:					# print each (key, value) pair
 			print(pair)
 
 	def alreadyExists(self, key):
@@ -15,12 +15,12 @@ class Array:								# ADT ~ Array of {key, value} arrays
 		return False
 
 	def insert(self, key, value):
-		if not self.alreadyExists(key):					# check to see if key isn't already in array
+		if not self.alreadyExists(key):				# check to see if key isn't already in array
 			return self.array.append((key, value))		# if true, add (key, value) pair to end of array
 
 	def remove(self, key):
-		for pair in self.array:						# iterate linearly
-			if pair[0] == key:						# find arg key at some index, call that index i
+		for pair in self.array:					# iterate linearly
+			if pair[0] == key:				# find arg key at some index, call that index i
 				return self.array.remove(pair)		# remove (key, value) pair @ index i
 		return 'Pair does not exist'
 
