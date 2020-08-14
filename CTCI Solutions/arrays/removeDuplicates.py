9e3e47bd436222a28b19a1c4c8be5c33a2d486a6
kamwithak <kamran.choudhry@mail.mcgill.ca>
2020-08-14 17:16:40 -0400

    EDIT: clean up

--- CTCI Solutions/arrays/removeDuplicates.py
+++ CTCI Solutions/arrays/removeDuplicates.py
@@ -5,7 +5,8 @@ Remove the duplicate elements in an unsorted n-array!
 
 '''
 ANS:
-
+1) count the number of occurences for each element using a dict
+2) if the # of occurences == 1: then add that particular element to the array we want to return
 '''
 
 '''
@@ -13,7 +14,7 @@ Time: O(n) where n is the number of elements in the n-array
 Space: O(n)
 '''
 
-def removeDups(arr): 
+def removeDups1(arr): 
     mp = {x : None for x in arr}               # values set to zero, all keys are unique by default
 
     _arr = []
@@ -22,6 +23,6 @@ def removeDups(arr):
 
     return _arr
 
-print(removeDups([1, 2, 5, 1, 7, 2, 4, 2, 4, 5, 1, -21]))
+print(removeDups1([1, 2, 5, 1, 7, 2, 4, 2, 4, 5, 1, -21]))
 
   
