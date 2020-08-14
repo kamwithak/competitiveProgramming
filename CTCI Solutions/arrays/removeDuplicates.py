4552e3ed7d786606ffa2df791d6d300ffa9d7abf
kamwithak <kamran.choudhry@mail.mcgill.ca>
2020-08-14 17:14:26 -0400

    EDIT: clean up

--- CTCI Solutions/arrays/removeDuplicates.py
+++ CTCI Solutions/arrays/removeDuplicates.py
@@ -1,6 +1,6 @@
 '''
 PROBLEM STATEMENT:
-Remove the duplicate elements in an unsorted array!
+Remove the duplicate elements in an unsorted n-array!
 '''
 
 '''
@@ -9,21 +9,19 @@ ANS:
 '''
 
 '''
-Time:
-Space:
+Time: O(n) where n is the number of elements in the n-array
+Space: O(n)
 '''
 
 def removeDups(arr): 
-    # mp = {}
-    # for x in arr:
-    #     mp[x] = 0
-    mp = {x : 0 for x in arr}               # values set to zero, by default, in map
+    mp = {x : None for x in arr}               # values set to zero, all keys are unique by default
 
-    for key in arr:                         # for each key in arr
-        if mp[key] == 0:                    # check if associatd value is equiv. to zero
-            print(key, end = " ") 
-            mp[key] = 1                     # if it is, then set the associatd value to one
+    _arr = []
 
-removeDups([ 1, 2, 5, 1, 7, 2, 4, 2 ]) 
+    for key in mp: _arr.append(key)
+
+    return _arr
+
+print(removeDups([1, 2, 5, 1, 7, 2, 4, 2, 4, 5, 1, -21]))
 
   
