fb166e599225775efde3581e9990e40e82420263
kamwithak <kamran.choudhry@mail.mcgill.ca>
2020-08-26 12:00:37 -0400

    NEW: question

---
+++ CTCI Solutions/linked lists/removeMiddleNode.py
@@ -0,0 +1,23 @@
+'''
+PROBLEM STATEMENT:
+
+'''
+
+'''
+ANS:
+
+'''
+
+class Node():
+    def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
+'''
+Time: O(n)
+Space: O(n)
+'''
+def kthToLast1(head: Node) -> Node:
+    return None
+
+if __name__ == "__main__":
+    pass
