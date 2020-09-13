aa5bc0ff980f8ae974249f459b3a3a2f38f130b6
Kam <none>
2020-09-13 00:26:13 -0400

    EDIT: clean up

--- ADT_Array.py
+++ ADT_Array.py
@@ -13,13 +13,13 @@ class Array:									# ADT ~ Array of {key, value} arrays
 		return False
 
 	def insert(self, key, value):
-		if not self.alreadyExists(key):				# check to see if key isn't already in array
+		if not self.alreadyExists(key):					# check to see if key isn't already in array
 			return self.array.append((key, value))		# if true, add (key, value) pair to end of array
 
 	def remove(self, key):
-		for pair in self.array:					# iterate linearly
-			if pair[0] == key:				# find arg key at some index, call that index i
-				return self.array.remove(pair)		# remove (key, value) pair @ index i
+		for pair in self.array:							# iterate linearly
+			if pair[0] == key:							# find arg key at some index, call that index i
+				return self.array.remove(pair)			# remove (key, value) pair @ index i
 		return 'Pair does not exist'
 
 	def update(self, key, value):
