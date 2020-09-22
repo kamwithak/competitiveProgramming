17fda3e4a3bac351e56bc0d081e115454eeb12ab
none <theblizzzzard@gmail.com>
2020-09-22 15:37:10 -0400

    EDIT: clean up

--- coinsChangeDP.py
+++ coinsChangeDP.py
@@ -17,10 +17,10 @@ def minCoins(coins, m, V):
     # Try every coin that has smaller value than V 
     for i in range(0, m): 
         if (coins[i] <= V):
-            sub_res = minCoins(coins, m, V-coins[i]) ``
+            sub_res = minCoins(coins, m, V-coins[i])
             # Check for INT_MAX to avoid overflow and see if 
             # result can minimized 
-            if (sub_res != sys.maxsize and sub_res + 1 < res):--
+            if (sub_res != sys.maxsize and sub_res + 1 < res):
                 ress.append(coins[i]) 
                 res = sub_res + 1
   
