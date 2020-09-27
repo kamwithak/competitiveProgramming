f546e3e14d491b45a72bd0a63ef2d3db8df4fa8b
none <theblizzzzard@gmail.com>
2020-09-27 15:50:40 -0400

    EDIT: clean up

--- maxProfit.py
+++ maxProfit.py
@@ -3,15 +3,21 @@ class MaxProfit():
         self.prices = prices
     
     def maxProfitOneTransaction(self):
-        prices = self.prices
-        curMax = 0
-        for i in range(0, len(prices)-1):
-            for j in range(i+1, len(prices)):
-                diff = prices[j] - prices[i]
-                if(diff > curMax):
-                    curMax = diff
-        return curMax
+        """
+        Time:O(N^2)
+        Space:O(1)
+        
+        Returns:
+            [int]: max profit with 1 transaction
+        """
+        curMaxProfit = 0
+        for i in range(0, len(self.prices)-1):
+            for j in range(i+1, len(self.prices)):
+                diff = self.prices[j] - self.prices[i]
+                if(diff > curMaxProfit):
+                    curMaxProfit = diff
+        return curMaxProfit
 
 if __name__ == "__main__":
-    obj = MaxProfit([7,6,4,3,1])
+    obj = MaxProfit([7,1,5,3,6,4])
     print(obj.maxProfitOneTransaction())
