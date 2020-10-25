7f6c4ac17d1e6496dc3cff339502c5b4bed8263a
none <theblizzzzard@gmail.com>
2020-10-24 23:14:08 -0400

    NEW: begin

---
+++ pig_latin.py
@@ -0,0 +1,19 @@
+'''
+PROBLEM STATEMENT:
+Design a program to take a word as an input, and then encode it into a Pig Latin.
+'''
+
+'''
+DESIGN:
+'''
+
+def toPigLatin(_str: str) -> str:
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    firstLetter = _str[0]
+    if (firstLetter in vowels):
+        return f"{_str}way"
+    return _str
+
+print(toPigLatin("amina"))
+
+
