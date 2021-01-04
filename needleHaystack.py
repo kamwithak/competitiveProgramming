8f5849828739f62b5142a68e0fe10364586d46aa
none <theblizzzzard@gmail.com>
2021-01-04 15:13:02 -0500

    NEW: Welcome back! 4 days to go

---
+++ needleHaystack.py
@@ -0,0 +1,37 @@
+class Solution():
+
+    def __init__(self, needle, haystack):
+        self.needle = needle
+        self.haystack = haystack
+
+    def needleInHaystack(self):
+        """
+        Given two strings, needle and haystack:
+        find the starting and ending index of needle in haystack
+        Ex) 'hello' & 'abchellodef' => [3, 7]
+        """
+        res = []
+        ptrN, ptrH, matchFound = 0, 0, False
+        
+        while (ptrH < len(self.haystack)):
+            newH = ptrH
+            newN = ptrN
+            while (not matchFound):
+                if (self.needle[newN] != self.haystack[newH]):
+                    break
+
+                newH += 1
+                newN += 1
+
+                if (newN == len(self.needle)):
+                    res.append(ptrH)
+                    res.append(newH-1)
+                    matchFound = True
+
+            ptrH += 1
+        return res
+
+
+
+obj = Solution('kam','iiikamkiii')
+print(obj.needleInHaystack())
---
+++ printAlternatively.py
@@ -0,0 +1,52 @@
+class Solution():
+
+	def __init__(self, A, B):
+		self.A = A
+		self.B = B
+
+	def printAlternativelySameSize(self):
+		"""
+		Assumes that len(self.A) == len(self.B) != 0
+		Alternatively print each element in the two Lists
+		"""
+		if (len(self.A) != len(self.B)):
+			raise Exception("the two lists must be of same length")
+		if (len(self.A) == len(self.B) == 0):
+			raise Exception("Empty lists")
+		#	
+		ptrA = 0 ; ptrB = 0 ; decisionPoint = False
+		while (ptrA < len(self.A) or ptrB < len(self.B)):
+			if (not decisionPoint):
+				print(self.A[ptrA])
+				ptrA+=1
+				decisionPoint = True
+			else:
+				print(self.B[ptrB])
+				ptrB+=1
+				decisionPoint = False
+
+	def printAlternativelyDifferentSize(self):
+		"""
+		Alternatively print each element in the two Lists, regardless of List size
+		"""
+		ptrA = 0 ; ptrB = 0 ; decisionPoint = False
+		while (ptrA < len(self.A) and ptrB < len(self.B)):
+			if (not decisionPoint):
+				print(self.A[ptrA])
+				ptrA+=1
+				decisionPoint = True
+			else:
+				print(self.B[ptrB])
+				ptrB+=1
+				decisionPoint = False
+
+		while (ptrA < len(self.A)):
+			print(self.A[ptrA])
+			ptrA += 1
+
+		while (ptrB < len(self.B)):
+			print(self.B[ptrB])
+			ptrB += 1
+
+obj = Solution(A=[3,2,1], B=[3,2,1])
+obj.printAlternativelySameSize()
