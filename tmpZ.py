b3ff8b5842beddd9abccf0d4a93c180c0dfd2355
kam <theblizzzzard@gmail.com>
2021-01-10 19:36:35 -0500

    nice

--- tmpZ.py
+++ tmpZ.py
@@ -17,3 +17,11 @@ for char in string:		# O(len(string) * len(_dict2.keys())) == O(n * m) where n i
 		_dict2[char] += 1
 
 print(_dict1 == _dict2)
+
+
+
+def high(x):
+    scores = {
+		'a':1,'b':2,'c':3,'d':4,'e':5,'f':6,'g':7,'h':8,'i':9,'j':10,'k':11,'l':12,'m':13,'n':14,'o':15,'p':16,'q':17,'r':18,'s':19,'t':20,'u':21,'v':22,'w':23,'x':24,'y':25,'z':26
+		}
+    print(x.split(' '))
