5dbe36f0ecca86d658163d720c3954b7235ccc0a
kamwithak <theblizzzzard@gmail.com>
2021-06-05 15:29:38 -0400

    new

--- toBinary.py
+++ toBinary.py
@@ -2,14 +2,21 @@ class toBinary:
     def __init__(self, binaryArr=[]):
         self.binaryArr = binaryArr
 
-    def conversion(self, n):
+    def conversionRecursive(self, n):
         if (n > 0):
-            self.conversion(int(n / 2))
+            self.conversionRecursive(int(n / 2))
             self.binaryArr.append(n % 2)
+    
+    def conversionIterative(self, n):
+        while (n > 0):
+            r = n % 2
+            self.binaryArr.append(r)
+            n = n // 2
+        self.binaryArr = self.binaryArr[::-1]
     
     def getBinaryArr(self):
         return self.binaryArr
 
 obj = toBinary()
-obj.conversion(3343)
+obj.conversionIterative(16)
 print(obj.getBinaryArr())
