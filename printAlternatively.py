65d563b7d663fe76438b0edad6ce5bca9b27a55d
kam <theblizzzzard@gmail.com>
2021-07-06 21:27:42 -0400

    up to date:

--- printAlternatively.py
+++ printAlternatively.py
@@ -51,4 +51,48 @@ class Solution():
 obj = Solution(A=[3,2,1], B=[3,2,1])
 obj.printAlternativelySameSize()
 
+"""
+
+Given two arrays, print each element alternatively
+
+For example)
+
+arr1 = [a,b,c,d]
+
+arr2 = [e,f,g,h,i,j,k]
+
+=> a e b f c g d h i j k
+
+"""
+
+class Solution():
+  def __init__(self, arr1, arr2):
+    self.arr1 = arr1
+    self.arr2 = arr2
+    self.n = len(self.arr1)
+    self.m = len(self.arr2)
+
+  def print_lists(self):
+    i, j = 0, 0
+    config = True
+    while(i < self.n and j < self.m):
+      if (config):
+        print(self.arr1[i])
+        i += 1
+        config = False
+      else:
+        print(self.arr2[j])
+        j += 1
+        config = True
+
+    while (i < self.n):
+      print(self.arr1[i])
+      i += 1
+
+    while (j < self.m):
+      print(self.arr2[j])
+      j += 1
+
+obj = Solution(['a', 'b', 'c', 'd'], ['e','f','g','h','i','j','k'])
+obj.print_lists()
 
