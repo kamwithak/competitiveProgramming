dc579e23c1b12f6715ac0015dc88b74a345250cb
kam <theblizzzzard@gmail.com>
2021-07-10 00:16:40 -0400

    nice: :

---
+++ colorGenerator.py
@@ -0,0 +1,21 @@
+import random
+
+def giveMeAColor():
+  possibleValues = '0123456789ABCDEF'
+  n = len(possibleValues)
+  arr = ['#']
+  for i in range(6):
+    arr.append(possibleValues[random.randint(0,255) % n])
+  return ''.join(arr) 
+
+print(giveMeAColor())
+
+
+n = 6
+for i in range(150):
+  if ((random.randint(0,255) % n) >= 6):
+    print('END - the compression law has been broken')
+    break
+  print(random.randint(0,255) % n)
+
+
