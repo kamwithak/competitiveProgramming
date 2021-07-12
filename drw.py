9be0b28f8d342353b0179c8c0d5dd884959af12e
kam <theblizzzzard@gmail.com>
2021-07-11 23:17:49 -0400

    updated

--- drw.py
+++ drw.py
@@ -1,7 +1,21 @@
-class Solution():
-    def __init__(self, master_object):
-        self.master_object = master_object
+def bubbleSort1(arr):
+    steps = 0
+    for i in range(0, len(arr)-1):					# k to n-2
+        for j in range(i+1, len(arr)):				# k+1 to n-1
+            steps += 1
+            if (arr[i] > arr[j]):
+                arr[j], arr[i] = arr[i], arr[j]
+        print(arr)
+    return arr
 
-if __name__ == "__main__":
-    obj = Solution({'name':'kamran','age':'23'})
-    print(obj.master_object)
+def bubbleSort2(arr):
+    n = len(arr)
+    for i in range(1, n):
+        for j in range(0, n-1):
+            print(f"pairs being examined: {arr[i], arr[j]}")
+            if (arr[j] > arr[j+1]):
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+
+
+print(bubbleSort1([9,8,7,6,5,4]))
