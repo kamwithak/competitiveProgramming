ff210c8601e1239aec3baab792c1ee5c5354e46b
kam <theblizzzzard@gmail.com>
2021-07-25 19:09:48 -0400

    up to date

--- reverse_list.py
+++ reverse_list.py
@@ -28,8 +28,17 @@ class ComplexListDataStructure():
         
         return self.arr
 
+    def reverse2(self):
+        if (len(self.arr) == 1):
+            return self.arr
+        else:
+            firstElement = self.arr.pop(0)
+            self.reverse2()
+            self.arr.append(firstElement)
+
+
 if __name__ == '__main__':
     obj = ComplexListDataStructure(arr=[1,2,3,4,5,6,7,8,9,10])
-    print(obj.reverse1())
-
+    obj.reverse2()
+    print(obj.arr)
 
