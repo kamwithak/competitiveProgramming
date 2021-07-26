5c69190900dcceac497c6351b057e1c6e6458b6a
kam <theblizzzzard@gmail.com>
2021-07-25 20:32:04 -0400

    nice

--- maximumSubArray.py
+++ maximumSubArray.py
@@ -1,14 +1,19 @@
 class maximumSubArraySum:
 	def __init__(self, arr):
 		self.arr = arr
-		
-	def bubbleSort(self):
-		for a in range(len(self.arr)):
-			for b in range(a, len(self.arr)):
+	
+	def sort(self):
+		for a in range(len(self.arr)-1):
+			for b in range(a+1, len(self.arr)):
 				if (self.arr[a] > self.arr[b]):
 					self.arr[b], self.arr[a] = self.arr[a], self.arr[b]
 		print(self.arr)
 
+	"""
+		[-2, 1, -3, 4, -1, 2, 1, -5, 4]
+		Look at all possible triplets: i, j, k
+		Compute sum for each, identify the max_sum using max()
+	"""
 	def cubicSolution(self):
 		bestSum = 0
 		for a in range(len(self.arr)):
@@ -19,7 +24,12 @@ class maximumSubArraySum:
 				bestSum = max(curSum, bestSum)
 		print(self.arr)
 		print("best sum: " + str(bestSum))
-		
+	
+	"""
+		[-2, 1, -3, 4, -1, 2, 1, -5, 4]
+		Look at all possible tuples i, j
+		Compute running sum for each pair i and j, identify the max_sum using max()
+	"""
 	def quadraticSolution(self):
 		bestSum = 0
 		for a in range(len(self.arr)):
@@ -30,6 +40,10 @@ class maximumSubArraySum:
 		print(self.arr)
 		print("best sum: " + str(bestSum))
 	
+	"""
+	Kadanes Algorithm:
+		[-2, 1, -3, 4, -1, 2, 1, -5, 4]
+	"""
 	def linearSolution(self):
 		bestSum = 0 ; curSum = 0
 		for a in range(len(self.arr)):
@@ -40,4 +54,5 @@ class maximumSubArraySum:
 
 if __name__ == '__main__':
 	obj = maximumSubArraySum([-1,2,4,-3,5,2,-5,2])
-	obj.linearSolution()
+	# obj.linearSolution()
+	obj.sort()
